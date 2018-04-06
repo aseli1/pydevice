a7e26280405ed579948853b872a76bc422bb885c
Anthony Seliga <anthony.seliga@gmail.com>
2018-04-06 10:56:49 -0400

    Add parameters to create_resource and update_resource

--- dm_requests/resource.py
+++ dm_requests/resource.py
@@ -5,9 +5,10 @@ class Resource():
 
     def __init__(self, args={}):
       self.api_key = args['api_key']
-      self.resource_id = str(args['resource_id'])
-      self.encoded_file = args['encoded_file']
+      self.resource_id = args.get('resource_id')
+      self.file_path = args['file_path']
       self.base_url = 'https://www.devicemagic.com/api/resources'
+      self.content_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 
     def all_resources(self):
         request = requests.get(self.base_url + ".json", auth=(self.api_key, 'pass')) # To get an overview of all the resources
@@ -21,18 +22,31 @@ class Resource():
         request = requests.get(self.base_url + "/"+ self.resource_id + "/describe.json", auth=(self.api_key, 'pass'))
         return request
 
-    def create_resource(self):
-        json = {"resource": {"description": "Services Agreement",
-                             "file": {"file_name": "Services.pdf",
+    def encode_file(self):
+        with open(self.file_path, "rb") as resource_file:
+            encoded_file = base64.b64encode(resource_file.read())
+            self.encoded_file = encoded_file
+
+    def create_resource(self, description, file_name, content_type=None):
+        if content_type == None:
+          content_type = self.content_type
+
+        self.encode_file()
+        json = {"resource": {"description": description,
+                             "file": {"file_name": file_name,
                                       "file_data": self.encoded_file,
-                                      "content_type": "application/pdf"}}}
+                                      "content_type": content_type}}}
         request = requests.post(self.base_url + ".json", auth=(self.api_key, 'pass'), json=json)
 
-    def update_resource(self):
-        json = {"resource": {"description": "Updated Agreement",
-                             "file": {"file_name": "Update_Services.pdf",
+    def update_resource(self, description, file_name, content_type=None):
+        if content_type == None:
+          content_type = self.content_type
+
+        self.encode_file()
+        json = {"resource": {"description": description,
+                             "file": {"file_name": file_name,
                                       "file_data": self.encoded_file,
-                                      "content_type": "application/pdf"}}}
+                                      "content_type": content_type}}}
         request = requests.put(self.base_url + "/" + self.resource_id, auth=(self.api_key, 'pass'), json=json)
 
     def delete_resource(self):
