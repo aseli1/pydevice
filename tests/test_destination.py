bde9075751f3e57ad445dd4cbc70f20a95ba7c71
Anthony Seliga <anthony.seliga@gmail.com>
2018-05-22 12:20:56 -0400

    Fix update and create tests

--- tests/test_destination.py
+++ tests/test_destination.py
@@ -4,32 +4,32 @@ import vcr
 
 dm = test_object
 
-@vcr.use_cassette('fixtures/cassettes/destination/all')
+@vcr.use_cassette('tests/cassettes/destination/all')
 def test_all():
     response = dm.destination.all()
     assert isinstance(response, dict)
 
-@vcr.use_cassette('fixtures/cassettes/destination/details')
+@vcr.use_cassette('tests/cassettes/destination/details')
 def test_details():
     response = dm.destination.details(test_destination['details_id'])
     assert isinstance(response, dict)
 
-@vcr.use_cassette('fixtures/cassettes/destination/create')
+@vcr.use_cassette('tests/cassettes/destination/create')
 def test_create():
-    response = dm.destination.create(test_destination['json'])
+    response = dm.destination.create(test_destination['create_json'])
     assert response, "Destination created"
 
-@vcr.use_cassette('fixtures/cassettes/destination/update')
+@vcr.use_cassette('tests/cassettes/destination/update')
 def test_update():
-    response = dm.destination.update(test_destination['update_id'], test_destination['json'])
+    response = dm.destination.update(test_destination['update_id'], test_destination['update_json'])
     assert response, "Destination updated"
 
-@vcr.use_cassette('fixtures/cassettes/destination/delete')
+@vcr.use_cassette('tests/cassettes/destination/delete')
 def test_delete():
     response = dm.destination.delete(test_destination['delete_id'])
     assert response, "Destination deleted"
 
-@vcr.use_cassette('fixtures/cassettes/destination/copy')
+@vcr.use_cassette('tests/cassettes/destination/copy')
 def test_copy():
     response = dm.destination.copy(test_destination['copy_id'])
     assert response, "Destination copied"
