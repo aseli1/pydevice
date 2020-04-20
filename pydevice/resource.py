250d16282eabacf5085eedbe10e454ee1a6f8f98
Anthony Seliga <anthony.seliga@gmail.com>
2020-04-19 20:10:59 -0400

    Updates for creating and updating resources

--- pydevice/resource.py
+++ pydevice/resource.py
@@ -23,30 +23,36 @@ class Resource():
 
     def __encode_file(self):
         with open(self.file_path, "rb") as resource_file:
-            encoded_file = base64.b64encode(resource_file.read())
+            encoded_file = base64.b64encode(resource_file.read()).decode('utf-8')
             self.encoded_file = encoded_file
 
-    def create(self, description, file_name):
-        self.__encode_file()
+    def create(self, description, file_name, file_data=None, content_type=None):
+        if file_data == None:
+            self.__encode_file()
+        data = self.encoded_file if file_data == None else file_data
+        content_type = self.content_type if content_type == None else content_type
         json = {"resource": {"description": description,
                              "file": {"file_name": file_name,
-                                      "file_data": self.encoded_file,
-                                      "content_type": self.content_type}}}
+                                      "file_data": data,
+                                      "content_type": content_type}}}
         request = self.r.post(self.base_url + ".json", json=json)
         if request.status_code >= 200 and request.status_code < 300:
-            return "Resource created"
+            return request.json()
         else:
             return "Failed with status code: {0}".format(request.status_code)
 
-    def update(self, resource_id, description, file_name):
-        self.__encode_file()
+    def update(self, resource_id, description, file_name, file_data=None, content_type=None):
+        if file_data == None:
+            self.__encode_file()
+        data = self.encoded_file if file_data == None else file_data
+        content_type = self.content_type if content_type == None else content_type
         json = {"resource": {"description": description,
                              "file": {"file_name": file_name,
-                                      "file_data": self.encoded_file,
-                                      "content_type": self.content_type}}}
+                                      "file_data": data,
+                                      "content_type": content_type}}}
         request = self.r.put(self.base_url + "/" + str(resource_id), json=json)
         if request.status_code >= 200 and request.status_code < 300:
-            return "Resource updated"
+            return r.json()
         else:
             return "Failed with status code: {0}".format(request.status_code)
 
