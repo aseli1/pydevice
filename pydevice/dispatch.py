f1b9a78e76335131b343ed100fd071b2f6f5aa8b
Anthony Seliga <anthony.seliga@gmail.com>
2020-04-21 21:10:43 -0400

    Updates dispatch to api v2

--- pydevice/dispatch.py
+++ pydevice/dispatch.py
@@ -3,39 +3,39 @@ class Dispatch():
     def __init__(self, session, org_id):
         self.r = session
         self.org_id = org_id
-        self.device_base_url = 'https://www.devicemagic.com/clients'
-        self.org_base_url = 'https://www.devicemagic.com/organizations'
+        self.device_base_url = 'https://www.devicemagic.com/api/v2/devices'
+        self.org_base_url = 'https://www.devicemagic.com/api/v2/organizations'
 
     def all(self, device_identifier=None):
         if device_identifier != None:
-            url = self.device_base_url + "/" + device_identifier + "/oneshots.json"
+            url = self.device_base_url + "/" + device_identifier + "/dispatches.json"
         else:
-            url = self.org_base_url + "/" + self.org_id + "/oneshots.json"
+            url = self.org_base_url + "/" + self.org_id + "/dispatches.json"
         request = self.r.get(url)
         return request.json()
 
     def push(self, device_identifier, json):
-        headers = {'Content-Type': 'application/json'}  
-        request = self.r.post(self.device_base_url + "/" + device_identifier + "/oneshots", data=json, headers=headers)
-        if request.status_code >= 200 and request.status_code < 300:
-            return "Dispatch successful"
-        else:
-            return "Failed with status code: {0}".format(request.status_code)
-    
-    def update(self, device_identifier, oneshot_id, json):
         headers = {'Content-Type': 'application/json'}
-        request = self.r.put(self.device_base_url + "/" + device_identifier + "/oneshots/" + str(oneshot_id), data=json, headers=headers)
+        request = self.r.post(self.device_base_url + "/" + device_identifier + "/dispatches", data=json, headers=headers)
         if request.status_code >= 200 and request.status_code < 300:
-            return "Dispatch updated"
+            return r.json()
         else:
             return "Failed with status code: {0}".format(request.status_code)
 
-    def delete(self, device_identifier, oneshot_id=None):
-        if oneshot_id != None:
-            url = self.device_base_url + "/" + device_identifier + "/oneshots/" + str(oneshot_id)
+    def update(self, device_identifier, dispatch_id, json):
+        headers = {'Content-Type': 'application/json'}
+        request = self.r.patch(self.device_base_url + "/" + device_identifier + "/dispatches/" + str(dispatch_id), data=json, headers=headers)
+        if request.status_code >= 200 and request.status_code < 300:
+            return r.json()
+        else:
+            return "Failed with status code: {0}".format(request.status_code)
+
+    def delete(self, device_identifier, dispatch_id=None):
+        if dispatch_id != None:
+            url = self.device_base_url + "/" + device_identifier + "/dispatches/" + str(dispatch_id)
             request = self.r.delete(url)
         else:
-            url = self.device_base_url + "/" + device_identifier + "/oneshots/destroy_all"
+            url = self.device_base_url + "/" + device_identifier + "/dispatches/destroy_all"
             request = self.r.post(url)
         if request.status_code >= 200 and request.status_code < 300:
             return "Dispatch deleted"
