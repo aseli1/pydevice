66baf7c311c7608f75f426d06856db6869b45473
Anthony Seliga <anthony.seliga@gmail.com>
2020-04-22 21:27:13 -0400

    Adds clone_xlsx

--- pydevice/resource.py
+++ pydevice/resource.py
@@ -21,16 +21,18 @@ class Resource():
         request = self.r.get(self.base_url + "/"+ str(resource_id) + "/describe.json")
         return request.json()
 
-    def __encode_file(self):
+    def __encode_file(self, file):
+        return base64.b64encode(file.read()).decode('utf-8')
+
+    def __encode_local_file(self):
         with open(self.file_path, "rb") as resource_file:
-            encoded_file = base64.b64encode(resource_file.read()).decode('utf-8')
-            self.encoded_file = encoded_file
+            self.encoded_file = self.__encoded_file(resource_file)
 
     def create(self, description, file_name, file_data=None, content_type=None):
         if file_data == None:
-            self.__encode_file()
-        data = self.encoded_file if file_data == None else file_data
-        content_type = self.content_type if content_type == None else content_type
+            self.__encode_local_file()
+        data = file_data or self.encoded_file
+        content_type = content_type or self.content_type
         json = {"resource": {"description": description,
                              "file": {"file_name": file_name,
                                       "file_data": data,
@@ -43,9 +45,9 @@ class Resource():
 
     def update(self, resource_id, description, file_name, file_data=None, content_type=None):
         if file_data == None:
-            self.__encode_file()
-        data = self.encoded_file if file_data == None else file_data
-        content_type = self.content_type if content_type == None else content_type
+            self.__encode_local_file()
+        data = file_data or self.encoded_file
+        content_type = content_type or self.content_type
         json = {"resource": {"description": description,
                              "file": {"file_name": file_name,
                                       "file_data": data,
@@ -55,6 +57,13 @@ class Resource():
             return r.json()
         else:
             return "Failed with status code: {0}".format(request.status_code)
+
+    def clone_xlsx(self, resource_id, description=None, file_name=None):
+        file = self.download(resource_id)
+        file_data = self.__encode_file(file)
+        description = description or "clone {0}".format(self.details(resource_id)["resource"]["description"])
+        file_name = file_name or "clone {0}".format(self.details(resource_id)["resource"]["original_filename"])
+        return create(description, file_name, file_data, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"):
 
     def delete(self, resource_id):
         request = self.r.delete(self.base_url + "/" + str(resource_id))
