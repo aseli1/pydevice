756ab15a82a64dd43dab02d8b225c916b20c75b4
Anthony Seliga <anthony.seliga@gmail.com>
2020-06-28 21:31:09 -0400

    Increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pydevice',
-    version='0.2.3',
+    version='0.2.4',
     description="Device Magic API Wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
